9d9ee1aafaeb705e91404b55e007232087b0cae4
Yar <Yar@Yars-Mac-2.local>
2015-09-16 10:04:20 +0300

    Initial Commit

---
+++ DYYStudyCoding/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DYYStudyCoding
+//
+//  Created by Yar on 9/16/15.
+//  Copyright (c) 2015 Yaroslav Driuchin. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
